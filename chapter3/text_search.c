82dc7efc9fbb16289f56d1a52d5b26fc7ec0bad7
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-08-23 08:55:42 +0300

    Add files via upload

--- chapter3/text_search.c
+++ chapter3/text_search.c
@@ -3,11 +3,11 @@
 
 char tracks[][80] = 
 	{
-		"я оставил свое сердце в √арвардской медицинской школе",
-		"Ќьюарк, Ќьюарк - город, полный чудес",
-		"“анец с мужланом",
-		"ќтсюда и до роодома",
-		"ƒевчонка с острова »водзима"
+		"я оставил свое сердце в Гарвардской медицинской школе",
+		"Ньюарк, Ньюарк - город, полный чудес",
+		"Танец с мужланом",
+		"Отсюда и до роддома",
+		"Девчонка с острова Эводзима"
 	};
 	
 void find_track(char search_for[])
@@ -16,14 +16,14 @@ void find_track(char search_for[])
 	
 	for (i=0; i<5; i++)
 		if (strstr(tracks[i], search_for))
-			printf("ѕесн¤ номер %i: '%s'\n", i, tracks[i]);
+			printf("Песня номер %i: '%s'\n", i, tracks[i]);
 }
 
 int main()
 {
 	char search_for[80];
 	
-	printf("»скать: ");
+	printf("Искать: ");
 	fgets(search_for, 80, stdin);
 	search_for[strlen(search_for)-1] = '\0';
 	find_track(search_for);

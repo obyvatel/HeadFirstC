78afdf9f38cb17c3c5f883b298780543bb682629
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-09-07 14:13:53 +0300

    Add files via upload

---
+++ chapter7/dear_johns.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+enum response_type {DUMP, SECOND_CHANCE, MARRIAGE};
+typedef struct
+{
+	char *name;
+	enum response_type type;
+} response;
+
+void dump(response r)
+{
+	printf("Дорогой %s,\n", r.name);
+	puts("К сожалению, Ваш недавний партнер по свиданию связался с нами,");
+	puts("чтобы сообщить, что Вы с ним больше не увидетесь.");
+}
+
+void second_chance(response r)
+{
+	printf("Дорогой %s,\n", r.name);
+	puts("Хорошие новости: Ваш недавний партнер по свиданию попросил нас");
+	puts("организовать еще одну встречу. Пожалуйста, перезвоните как можно скорее.");
+}
+
+void marriage(response r)
+{
+	printf("Дорогой %s,\n", r.name);
+	puts("Поздравляем! Ваш недавний партнер по свиданию");
+	puts("связался с нами с предложением о браке.");
+}
+
+// создаем массив указателей на функции
+void (*replies[])(response) = {dump, second_chance, marriage};
+
+int main()
+{
+	response r[] = { 
+		{"Майк", DUMP}, {"Луис", SECOND_CHANCE}, {"Мэттью", SECOND_CHANCE}, {"Уильям", MARRIAGE} 
+	};
+	int i;
+	
+	for (i=0; i<4; i++)
+	{
+		(replies[r[i].type])(r[i]);
+	}
+	
+	return 0;
+}

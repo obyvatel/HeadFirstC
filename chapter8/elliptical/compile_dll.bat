1478c0df3e5018ea1dace28e59b0661d30150d55
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-09-10 11:08:58 +0300

    Add files via upload

---
+++ chapter8/elliptical/compile_dll.bat
@@ -0,0 +1,7 @@
+﻿gcc -Ic:\includes\ -c elliptical.c -o elliptical.o
+gcc elliptical.o -Lc:\libs -lhfcal  -o elliptical
+
+echo запуск. PATH не сработал, пришлось hfcal.dll 
+echo переписать в папку с исполнямым файлом
+PATH="%PATH%;c:\libs\"
+elliptical

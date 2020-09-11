53216d980c6ba5ef886bfa751563694fd09fa366
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-09-11 10:07:16 +0300

    Add files via upload

---
+++ chapter8/compile_elliptical.bat
@@ -0,0 +1,16 @@
+﻿echo создать hfcal_UK.o
+echo hfcal.h ищет в c:\includes
+gcc -Ic:\includes -c hfcal_UK.c -o hfcal.o
+
+echo конвертирование объектного файла в dll (dynamic link library)
+gcc -shared hfcal.o -o c:\libs\hfcal.dll
+
+echo создание treadmill.exe
+gcc -Ic:\includes\ -c elliptical.c -o elliptical.o
+gcc elliptical.o -Lc:\libs -lhfcal  -o elliptical
+
+echo копирование hfcal.dll в текущую папку
+copy c:\libs\hfcal.dll .\hfcal.dll
+
+echo запуск
+elliptical

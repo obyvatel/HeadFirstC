2b01f6fc95896659b4a711fb48b69bbe6a2a1a26
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-09-15 15:06:42 +0300

    Delete compile.bat

--- chapter9/python/compile.bat
+++
@@ -1,7 +0,0 @@
-echo создать переменную среды RSS_FEED
-set RSS_FEED=http://rss.cnn.com/rss/edition.rss
-rem set RSS_FEED=https://habr.com/ru/rss/flows/develop/all/?fl=ru
-rem set RSS_FEED=http://rss.cnn.com/rss/edition.rss
-echo запустить python скрипт
-python rssgossip.py trump
-rem python rssgossip.py google

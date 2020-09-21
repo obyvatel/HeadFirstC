dc3fcdd71524aa8e127969efc0bc8eaefd03cf8f
obyvatel <48647650+obyvatel@users.noreply.github.com>
2020-09-21 19:58:19 +0300

    Add files via upload

---
+++ chapter1/cards__4_71.c
@@ -0,0 +1,53 @@
+/*
+* Программа для оценивания важности карт
+* Выпущено под публичной Лас-Вегасской лицензией.
+* (с) 2014 Команда колледжа по блек-джеку
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char card_name[3];
+	int count = 0;
+	
+	while (card_name[0] != 'X')
+	{
+		puts("Введите название карты: ");
+		scanf("%2s", card_name);
+		int val = 0;
+	
+		switch (card_name[0])
+		{
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A' : val = 11; break;
+			case 'X' : continue;
+			default : 
+				val = atoi(card_name);
+				if ((val<1) || (val>10))
+				{
+					puts("Я не понимаю это значение!");
+					continue;
+				}
+		}
+		
+		if ((val > 2) && (val < 7))
+		{
+			count++;
+		}
+		else if (val == 10)
+		{
+			count--;
+		}
+		
+		printf("Текущий счет %i\n", count);
+	}
+	
+	return 0;
+	
+}
